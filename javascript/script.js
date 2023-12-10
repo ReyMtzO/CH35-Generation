79afec63458192c44b1f1b1af2f2262b6015f728
ReyMtzO <reyjazzmtz@gmail.com>
2023-12-10 12:43:44 -0600

    Modificaciones en archivo script.js, en práctica de JS

--- javascript/script.js
+++ javascript/script.js
@@ -18,7 +18,9 @@ console.log("La resta de ambos números es: " + (number1 - number2));
 console.log("La multiplicación de ambos números es: " + (number1 * number2));
 console.log("La división de ambos números es: " + (number1 / number2)); */
 
-var edad = prompt ("Bienvenido al bar. Ingresa tu edad");
+
+// Ejercicio en clase con condicionales (ejemplo de restricciones para entrada a un bar)
+/*var edad = prompt ("Bienvenido al bar. Ingresa tu edad");
 
 if (edad < 18) {
     alert("Bebecito, No puedes pasar");
@@ -28,3 +30,27 @@ else if (edad >= 18 && edad <= 65) {
 }else{
     alert("Demasiado viejo hermano, No puedes pasar");
 }
+*/
+
+
+//TIPOS DE DATOS Y VARIABLES
+
+//scope es el alcance de nuestras variables
+
+/* var, let, const. La diferencia es el alcance 
+var - tiene un alcance global
+let - tiene un alcnace local
+const - se mantiene sin ningun cambio */
+
+/* string:cadena de texto (lleva comillas)
+number: numero
+boolena: true/false
+null: nulo
+undefinied:no esta asigando o definido */
+
+let nombre = "Daniel";
+let invitadosExtra = 2;
+let esMayorEdad = true;
+let edad = 24;
+let invitadoEspecial = null;
+let horaDeSalida = undefined;
