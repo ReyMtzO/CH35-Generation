4e031281a17c222f304e06d5eee386322aa60aaa
ReyMtzO <reyjazzmtz@gmail.com>
2024-01-23 01:01:51 -0600

    Trabajos de práctica aplicando fundamentos de POO

---
+++ JavaGeneration/poo1/src/org/generation/employee/EmployeeMain.java
@@ -0,0 +1,32 @@
+package org.generation.employee;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		//Instanciar los objetos
+		Employee Daniel = new Employee("Daniel", "Maldonado", 7836, 8596.33d, "Instructor");
+		Employee Abigail = new Employee("Abigail", "Martínez", 5274, 1975.20d, "Desarrollador frontend");
+		Employee Arturo = new Employee("Arturo", "Ávila", 4483, 17999.62, "Desarrollador backend");
+	
+		System.out.println(Daniel);
+		System.out.println(Abigail);
+		System.out.println(Arturo);
+		
+		//Invocando métodos definidos previamente (trabajar, calcularSalario, capacitarse, infoGeneral)
+		Daniel.infoGeneral();
+		Arturo.calcularSalario();
+		Abigail.capacitarse();
+		
+		//Utilizar información específica de cada objeto mediante sus propiedades
+		//Usando getter
+		System.out.println(Daniel.getNombre() + " " + Daniel.getApellido() + " ocupa el puesto de " + Daniel.getPuesto());
+		//Accediendo a la propiedad del objeto
+		System.out.println(Abigail.puesto + " gana $" + Abigail.salario + " peosos");
+		
+		//Actualizando información mediante Setter
+		Arturo.setSalario(24598.11);
+		Arturo.calcularSalario();
+		
+		
+	}
+}
---
+++ JavaGeneration/poo1/src/org/generation/letras/Letras.java
@@ -0,0 +1,24 @@
+package org.generation.letras;
+
+import java.util.Scanner;
+
+/*
+ * Crear un programa que le solicite un mensaje al usuario y cuente cuántas vocales, consonantes, números y símbolos posee dicho mensaje.
+ * 		Letras: métodos para interactuar con el usuario
+ * 		LetrasMain: vamos a instanciar los objetos
+ * 		Contador: métodos para contar vocales, consonantes, número y caracteres
+ */
+public class Letras {
+	//Para que nuestro usuario interactúe, definimos un Scanner
+	Scanner scanner = new Scanner(System.in);
+	
+	//Metemos el Scanner dentro de un método
+	public String leerEntrada() {
+		return scanner.nextLine();
+	}
+	
+	//Método para proporcionar contexto al usuario
+	public void mostrarMensaje(String mensaje) {
+		System.out.println(mensaje);
+	}
+}
